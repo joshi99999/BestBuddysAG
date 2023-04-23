079bc5f1e9665c9613b8c948002c5ef8c49b173a
Sebastian <sebastian.riemann@stud.hs-kempten.de>
2023-04-23 18:42:47 +0200

    scaleDetection.py updated

--- scaleDetection.py
+++ scaleDetection.py
@@ -1,6 +1,5 @@
 import cv2
 import numpy as np
-from time import time
 
 org = cv2.imread(filename="Repository/image.jpg")
 org = cv2.resize(src=org, dsize=(960, 540))
@@ -11,8 +10,6 @@ lsd = cv2.createLineSegmentDetector()
 lines = np.reshape(lsd.detect(img)[0], (-1, 2, 2))
 lengths = np.hypot(lines[:,0,0] - lines[:,1,0], lines[:,0,1] - lines[:,1,1])
 centers = lines[:,0] + (lines[:,1] - lines[:,0]) / 2
-
-before = time()
 
 indexes = np.arange(0, len(lines), 1)
 
@@ -49,12 +46,4 @@ for i in range(len(lines)):
         color = cv2.line(img=color, pt1=(int(lines[i,1,0]), int(lines[i,1,1])), pt2=(int(parallel[1,0]), int(parallel[1,1])), color=(0, 255, 255), thickness=1)
         color = cv2.line(img=color, pt1=(int(parallel[0,0]), int(parallel[0,1])), pt2=(int(parallel[1,0]), int(parallel[1,1])), color=(0, 255, 255), thickness=1)
         cv2.imshow("Bearbeitet", color)
-        cv2.waitKey(0)
-
-
-
-
-print(time() - before)
-
-cv2.imshow("Bearbeitet", color)
-cv2.waitKey(0)
+        cv2.waitKey(1)
