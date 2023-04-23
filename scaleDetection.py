744a2b98e22a52ce84a193efd9adfc6e16f0362a
Sebastian <sebastian.riemann@stud.hs-kempten.de>
2023-04-23 18:41:17 +0200

    scaleDetection.py updated

--- scaleDetection.py
+++ scaleDetection.py
@@ -14,6 +14,8 @@ centers = lines[:,0] + (lines[:,1] - lines[:,0]) / 2
 
 before = time()
 
+indexes = np.arange(0, len(lines), 1)
+
 
 for i in range(len(lines)):
     color = org.copy()
@@ -25,15 +27,32 @@ for i in range(len(lines)):
     color = cv2.circle(img=color, center=(int(centers[i][0]), int(centers[i][1])), radius=int(lengths[i]), color=(0,255,0), thickness=1)
 
     vectors = centers - centers[i]
-    near = centers[np.hypot(vectors[:,0], vectors[:,1]) < lengths[i]]
+    
+    near = indexes[np.hypot(vectors[:,0], vectors[:,1]) < lengths[i]]
 
     for n in near:
-        
-        color = cv2.circle(img=color, center=(int(n[0]),int(n[1])), radius=2, color=(0,0,255), thickness=-1)
+
+        vector1 = lines[i,1] - lines[i,0]
+        vector2 = lines[n,1] - lines[n,0]
+
+        vector3 = vector1[[1,0]] * [1,-1]
 
 
-    #cv2.imshow("Bearbeitet", color)
-    #cv2.waitKey(0)
+        parallel = (lines[i] + vector3) if 0 < np.dot(centers[n] - centers[i], vector3) else (lines[i] - vector3)
+
+        M = cv2.getPerspectiveTransform(np.concatenate((lines[i], lines[n] if 0 < np.dot(vector1, vector2) else lines[n,[1,0]]), dtype=np.float32), np.concatenate((lines[i], parallel), dtype=np.float32))
+
+        print(M)
+
+        color = cv2.circle(img=color, center=(int(centers[n][0]),int(centers[n][1])), radius=2, color=(0,0,255), thickness=-1)
+        color = cv2.line(img=color, pt1=(int(lines[i,0,0]), int(lines[i,0,1])), pt2=(int(parallel[0,0]), int(parallel[0,1])), color=(0, 255, 255), thickness=1)
+        color = cv2.line(img=color, pt1=(int(lines[i,1,0]), int(lines[i,1,1])), pt2=(int(parallel[1,0]), int(parallel[1,1])), color=(0, 255, 255), thickness=1)
+        color = cv2.line(img=color, pt1=(int(parallel[0,0]), int(parallel[0,1])), pt2=(int(parallel[1,0]), int(parallel[1,1])), color=(0, 255, 255), thickness=1)
+        cv2.imshow("Bearbeitet", color)
+        cv2.waitKey(0)
+
+
+
 
 print(time() - before)
 
