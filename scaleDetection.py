d9ba896bce893138d6101d3b9b87b19f0788b21e
Sebastian <sebastian.riemann@stud.hs-kempten.de>
2023-04-27 11:11:22 +0200

    scaleDetection.py updated

--- scaleDetection.py
+++ scaleDetection.py
@@ -15,13 +15,15 @@ indexes = np.arange(0, len(lines), 1)
 
 
 for i in range(len(lines)):
+    if img.shape[0] / 10 < lengths[i]:
+        continue
     color = org.copy()
     for j in range(len(lines)):
         line = lines[j]
         center = (int(centers[j][0]), int(centers[j][1]))
-        color = cv2.line(img=color, pt1=(int(line[0,0]), int(line[0,1])), pt2=(int(line[1,0]), int(line[1,1])), color=(0, 0, 255), thickness=1)
+        color = cv2.line(img=color, pt1=line[0].astype(np.int32), pt2=line[1].astype(np.int32), color=(0, 0, 255), thickness=1)
         color = cv2.circle(img=color, center=center, radius=1, color=(255,0,0), thickness=-1)
-    color = cv2.circle(img=color, center=(int(centers[i][0]), int(centers[i][1])), radius=int(lengths[i]), color=(0,255,0), thickness=1)
+    color = cv2.circle(img=color, center=centers[i].astype(np.int32), radius=int(lengths[i]), color=(0,255,0), thickness=1)
 
     vectors = centers - centers[i]
     
@@ -34,16 +36,23 @@ for i in range(len(lines)):
 
         vector3 = vector1[[1,0]] * [1,-1]
 
+        if 0 < np.dot(centers[n] - centers[i], vector3):
+            parallel1 = lines[i] + vector3
+            parallel2 = lines[i] - vector3
+        else:
+            parallel1 = lines[i] - vector3
+            parallel2 = lines[i] + vector3
 
-        parallel = (lines[i] + vector3) if 0 < np.dot(centers[n] - centers[i], vector3) else (lines[i] - vector3)
+        M = cv2.getPerspectiveTransform(np.concatenate((lines[i], (lines[n] if 0 < np.dot(vector1, vector2) else lines[n,[1,0]])), dtype=np.float32), np.concatenate((lines[i], parallel1), dtype=np.float32))
 
-        M = cv2.getPerspectiveTransform(np.concatenate((lines[i], lines[n] if 0 < np.dot(vector1, vector2) else lines[n,[1,0]]), dtype=np.float32), np.concatenate((lines[i], parallel), dtype=np.float32))
+        parallel2homogen = np.insert(parallel2, 2, 1, axis=1)
 
-        print(M)
+        parallel2warped = (parallel2homogen @ M[[0,1]].T) / np.reshape(parallel2homogen @ M[2].T, (2,1))
 
-        color = cv2.circle(img=color, center=(int(centers[n][0]),int(centers[n][1])), radius=2, color=(0,0,255), thickness=-1)
-        color = cv2.line(img=color, pt1=(int(lines[i,0,0]), int(lines[i,0,1])), pt2=(int(parallel[0,0]), int(parallel[0,1])), color=(0, 255, 255), thickness=1)
-        color = cv2.line(img=color, pt1=(int(lines[i,1,0]), int(lines[i,1,1])), pt2=(int(parallel[1,0]), int(parallel[1,1])), color=(0, 255, 255), thickness=1)
-        color = cv2.line(img=color, pt1=(int(parallel[0,0]), int(parallel[0,1])), pt2=(int(parallel[1,0]), int(parallel[1,1])), color=(0, 255, 255), thickness=1)
+        color = cv2.circle(img=color, center=centers[n].astype(np.int32), radius=2, color=(0,0,255), thickness=-1)
+        color = cv2.line(img=color, pt1=lines[i,0].astype(np.int32), pt2=parallel1[0].astype(np.int32), color=(0, 255, 255), thickness=1)
+        color = cv2.line(img=color, pt1=lines[i,1].astype(np.int32), pt2=parallel1[1].astype(np.int32), color=(0, 255, 255), thickness=1)
+        color = cv2.line(img=color, pt1=parallel1[0].astype(np.int32), pt2=parallel1[1].astype(np.int32), color=(0, 255, 255), thickness=1)
+        color = cv2.line(img=color, pt1=parallel2warped[0].astype(np.int32), pt2=parallel2warped[1].astype(np.int32), color=(255,0,0), thickness=1)
         cv2.imshow("Bearbeitet", color)
         cv2.waitKey(1)
