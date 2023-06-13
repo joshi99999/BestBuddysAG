2047c1d85c0fdd911f3659aafd2602844bf15a77
Sebastian <sebastian.riemann@stud.hs-kempten.de>
2023-06-14 00:50:34 +0200

    Commit 2023.06.14 00:50

--- camera/camera/camera.py
+++ camera/camera/camera.py
@@ -18,10 +18,10 @@ class Camera(Node):
         self.stream = self.create_publisher(Image, 'camera_stream', 5)
         self.timer = self.create_timer(1/framerate, self.main)
         #self.camera = cv2.VideoCapture(0, cv2.CAP_V4L2)
-        self.detector = ScaleDetector(delta1=20, delta2=5, n=20, phi1=0.02, phi2=0.005, m=20, count=20, accuracy=0.5)
+        self.detector = ScaleDetector(delta1=20, delta2=5, n=20, phi1=0.02, phi2=0.005, sigma1=0.01, sigma2=0.001, m=20, count=20, accuracy=0.5)
         self.shape = (1000, 200)
         self.bridge = CvBridge()
-        self.i = 1
+        self.i = 0
 
     def main(self):
         #ret, original = self.camera.read()
@@ -32,23 +32,23 @@ class Camera(Node):
         self.i = self.i + 1 if self.i < 12 else 1
 
         print(getcwd())
-
+        print(self.i)
         original = cv2.imread("src/camera/images/image" + str(self.i) + ".jpg")
         
         gray = cv2.cvtColor(src=original, code=cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(src=gray, ksize=(5,5), sigmaX=0)
         binary = cv2.threshold(src=gray, thresh=150, maxval=255, type=cv2.THRESH_BINARY+cv2.THRESH_OTSU)[1]
 
-        corners = self.detector.detectScale(img=binary)
+        corners = self.detector.detectScale(img=binary, org=original)
         if corners is None:
             return
         
+        corners[2] += 9*(corners[2]-corners[3])//2
         corners[0] += 9*(corners[0]-corners[1])//2
-        corners[3] += 9*(corners[3]-corners[2])//2
 
         print(original.shape)
 
-        M = cv2.getPerspectiveTransform(corners, float32([[self.shape[0], self.shape[1]], [self.shape[0], 0], [0, 0], [0, self.shape[1]]]))
+        M = cv2.getPerspectiveTransform(corners, float32([[0, self.shape[1]], [0, 0], [self.shape[0], self.shape[1]], [self.shape[0], 0]]))
         belt = cv2.warpPerspective(src=original, M=M, dsize=self.shape)
         cv2.imshow("Belt", belt)
         cv2.waitKey(1)
--- camera/camera/scaleDetection.py
+++ camera/camera/scaleDetection.py
@@ -18,18 +18,22 @@ class ScaleDetector:
     #  @param n Number of orthogonal steps done in each direction while scanning the nearby area.
     #  @param phi1 Angle step width while the whole image is scanned.
     #  @param phi2 Angle step width while the nearby area of a valid line is scanned.
+    #  @param sigma1 Start angle step witdh for the border adaption.
+    #  @param sigma2 Minimal angle step witdh for the border adaption.
     #  @param m Number of angle steps done in each direction while scanning the nearby area.
     #  @param count Number of gray value edges with minimum absolute distance and low varity in distance ratio in a row, 
     #               the line must go trough so it is assumed to go through the scale.
     #  @param accuracy The maximum value the ratio between the distances from an edge to the previous edge and the next edge can deviate from one.
     #  @param min The minimum distance between two edges.
 
-    def __init__(self, delta1, delta2, n, phi1, phi2, m, count, accuracy):
+    def __init__(self, delta1, delta2, n, phi1, phi2, sigma1, sigma2, m, count, accuracy):
         self.__delta1 = delta1
         self.__delta2 = delta2
         self.__n = n
         self.__phi1 = phi1
         self.__phi2 = phi2
+        self.__sigma1 = sigma1
+        self.__sigma2 = sigma2
         self.__m = m
         self.__count = count
         self.__accuracy = accuracy
@@ -45,7 +49,7 @@ class ScaleDetector:
         self.__radius += self.__delta1
         if self.__maxRadius < self.__radius:
             self.__radius = self.__delta1
-            self.__alpha = self.__phi1 - self.__alpha if 0 < self.__alpha else - self.__alpha
+            self.__alpha = self.__phi1 - self.__alpha if 0 < self.__alpha else -self.__alpha
             if abs(self.__alpha) < self.__phi1:
                 self.__alpha = -pi/2
                 self.__maxRadius = 0
@@ -67,37 +71,37 @@ class ScaleDetector:
         i = np.arange(0,vectors.shape[0],1)[vectors[:,1] != 0]
         if 0 < i.shape[0]:
             s = (anchors[i,0]*vectors[i,1]-anchors[i,1]*vectors[i,0])/vectors[i,1]
-            i = i[np.logical_and(0<=s, s<shape[1])]
+            i = i[np.logical_and(0<=s, s<shape[1]-1)]
             if 0 < i.shape[0]:
-                ends[i,j[i],0] = s[i].astype(np.uint16)
+                ends[i,j[i],0] = s[i]
                 ends[i,j[i],1] = 0
                 j[i] += 1
 
         i = np.arange(0,vectors.shape[0],1)[vectors[:,0] != 0]
         if 0 < i.shape[0]:
-            s = (anchors[i,1]*vectors[i,0]+(shape[1]-anchors[i,0])*vectors[i,1])/vectors[i,0]
-            i = i[np.logical_and(0<=s, s<shape[0])]
+            s = (anchors[i,1]*vectors[i,0]+(shape[1]-1-anchors[i,0])*vectors[i,1])/vectors[i,0]
+            i = i[np.logical_and(0<=s, s<shape[0]-1)]
             if 0 < i.shape[0]:
-                ends[i,j[i],0] = shape[1]
-                ends[i,j[i],1] = s[i].astype(np.uint16)
+                ends[i,j[i],0] = shape[1]-1
+                ends[i,j[i],1] = s[i]
                 j[i] += 1
 
         i = np.arange(0,vectors.shape[0],1)[np.logical_and(vectors[:,1] != 0, j < 2)]
         if 0 < i.shape[0]:
-            s = (anchors[i,0]*vectors[i,1]-(anchors[i,1]-shape[0])*vectors[i,0])/vectors[i,1]
-            i = i[np.logical_and(0<s, s<=shape[1])]
+            s = (anchors[i,0]*vectors[i,1]-(anchors[i,1]-shape[0]+1)*vectors[i,0])/vectors[i,1]
+            i = i[np.logical_and(0<s, s<=shape[1]-1)]
             if 0 < i.shape[0]:
-                ends[i,j[i],0] = s[i].astype(np.uint16)
-                ends[i,j[i],1] = shape[0]
+                ends[i,j[i],0] = s[i]
+                ends[i,j[i],1] = shape[0]-1
                 j[i] += 1
 
         i = np.arange(0,vectors.shape[0],1)[np.logical_and(vectors[:,0] != 0, j < 2)]
         if 0 < i.shape[0]:
             s = (anchors[i,1]*vectors[i,0]-anchors[i,0]*vectors[i,1])/vectors[i,0]
-            i = i[np.logical_and(0<s, s<=shape[0])]
+            i = i[np.logical_and(0<s, s<=shape[0]-1)]
             if 0 < i.shape[0]:
                 ends[i,j[i],0] = 0
-                ends[i,j[i],1] = s[i].astype(np.uint16)
+                ends[i,j[i],1] = s[i]
         
         return ends
 
@@ -109,7 +113,7 @@ class ScaleDetector:
         lineVector = ends[0]-ends[1]
         angle = np.arctan2(lineVector[1],lineVector[0])
         length = np.ceil(np.linalg.norm(lineVector))
-        return ([cos(angle), sin(angle)] * np.reshape(np.arange(0, length+1, 1, dtype=np.int16), (-1,1)) + ends[1]).astype(np.uint16)
+        return np.rint([cos(angle), sin(angle)] * np.reshape(np.arange(0, length+1, 1, dtype=np.int16), (-1,1)) + ends[1]).astype(np.uint16)
 
     ## Calculates the positions of the edges between black and white in the binary gray values of a line and 
     ## checks on basis of the distances between them, if a line is likely to go through <count> squares of the scale.
@@ -139,7 +143,7 @@ class ScaleDetector:
         anchorVectors = np.stack((np.cos(angles), np.sin(angles)), axis=-1)
         lineVectors = anchorVectors[:,::-1]*[1,-1]
         anchors = center + np.reshape(np.outer(np.linspace(-self.__n*self.__delta2, self.__n*self.__delta2, 2*self.__n+1), anchorVectors), (-1,2))
-        ends = ScaleDetector.calculateLineEnds(shape=(self.__img.shape[0]-1,self.__img.shape[1]-1), anchors=anchors, vectors=np.tile(lineVectors, (2*self.__n+1,1)))
+        ends = ScaleDetector.calculateLineEnds(shape=self.__img.shape, anchors=anchors, vectors=np.tile(lineVectors, (2*self.__n+1,1)))
         line = None
         for e in ends:
             edges = self.analyseLine(ends=e)
@@ -158,7 +162,7 @@ class ScaleDetector:
             while phi < pi/2:
                 phi = phi+pi/720 if i%2 == 0 else phi-pi/720
                 vector = np.array([[cos(angle+phi), sin(angle+phi)]], dtype=np.float32)
-                ends = ScaleDetector.calculateLineEnds(shape=(self.__img.shape[0]-1,self.__img.shape[1]-1), anchors=np.array([left], dtype=np.uint16) if i<2 else np.array([right], dtype=np.uint16), vectors=vector)[0]
+                ends = ScaleDetector.calculateLineEnds(shape=self.__img.shape, anchors=np.array([left], dtype=np.uint16) if i<2 else np.array([right], dtype=np.uint16), vectors=vector)[0]
                 edges = self.analyseLine(ends=ends)
                 if edges is not None:
                     edges = edges[left[0] <= edges[:,0] if i < 2 else edges[:,0] <= right[0]]
@@ -168,18 +172,58 @@ class ScaleDetector:
                 break
         return corners.astype(np.float32)
 
-    def detectScale(self, img):
+    def adaptBorders(self, left, right, org):
+        angle = np.arctan2(right[1]-left[1], right[0]-left[0])
+        angles = np.array([angle, angle], dtype=np.float32)
+        corners = np.array([left, left, right, right], dtype=np.int16)
+        stepWidths = np.array([self.__sigma1, -self.__sigma1, -self.__sigma1, self.__sigma1], dtype=np.float32)
+        i = -1        
+        snap = org.copy()
+        while 0 < np.sum(abs(stepWidths)):
+            snap = org.copy()
+            cv2.line(img=snap, pt1=corners[1], pt2=corners[3], color=(0,0,25), thickness=1)
+            cv2.line(img=snap, pt1=corners[0], pt2=corners[2], color=(0,0,255), thickness=1)
+            cv2.circle(img=snap, center=corners[0], radius=5, color=(0,255,0), thickness=-1)
+            cv2.imshow("Snap", snap)
+            cv2.waitKey(0)
+            i = (i + 1) % 4
+            if stepWidths[i] == 0:
+                continue
+            angle = angles[i%2]+stepWidths[i]
+            vector = np.array([[cos(angle), sin(angle)]], dtype=np.float32)
+            print(corners[i])
+            ends = ScaleDetector.calculateLineEnds(shape=self.__img.shape, anchors=corners[i:i+1], vectors=vector)[0]
+            edges = self.analyseLine(ends=ends)
+            if edges is not None:
+                edges = edges[corners[i,0]-1 <= edges[:,0] if i < 2 else edges[:,0] <= corners[i,0]+1]
+                if self.__count < edges.shape[0] and (np.linalg.norm(corners[i] - edges[0]) < 2 or np.linalg.norm(corners[i] - edges[-1]) < 2):
+                    print(edges[0])
+                    print(edges[-1])
+                    newCorner = edges[self.__count if i < 2 else 0] if edges[0,0] < edges[-1,0] else edges[-self.__count - 1 if i < 2 else -1]
+                    if np.any(corners[(i + 2) % 4] != newCorner):
+                        corners[(i + 2) % 4] = newCorner
+                        angles[i%2] = angle
+                        continue
+            if stepWidths[i] == -stepWidths[(i + 2) % 4] or abs(stepWidths[i] / 2) < self.__sigma2:
+                stepWidths[i] = 0
+            else:
+                stepWidths[i] /= 2
+                stepWidths[(i + 2) % 4] = -stepWidths[i]
+        return corners.astype(np.float32)
+
+    def detectScale(self, img, org):
         self.__img = img
         self.__min = img.shape[1] / self.__count / 2.5
         while self.__nextLine():
             anchor = self.__radius * np.array([cos(self.__alpha), sin(self.__alpha)], dtype=np.float32)
             if self.__alpha < 0:
                 anchor = anchor + [0, img.shape[0]]
-            ends = ScaleDetector.calculateLineEnds(shape=(img.shape[0]-1,img.shape[1]-1), anchors=np.array([anchor]), vectors=np.array([[sin(self.__alpha), -cos(self.__alpha)]], dtype=np.float32))[0]
+            ends = ScaleDetector.calculateLineEnds(shape=img.shape, anchors=np.array([anchor]), vectors=np.array([[sin(self.__alpha), -cos(self.__alpha)]], dtype=np.float32))[0]
             line = self.analyseLine(ends=ends)
             if line is not None:
                 left = self.scanArea(center=line[0]+(line[self.__count]-line[0])//2 if line[0,0] < line[-1,0] else line[-1]+(line[-self.__count-1]-line[-1])//2)
                 if left is not None:
+                    return self.adaptBorders(left=left[0 if left[0,0] < left[-1,0] else -1].astype(np.int16), right=left[self.__count if left[0,0] < left[-1,0] else -self.__count-1].astype(np.int16), org=org)
                     return self.findCorners(left=left[0 if left[0,0] < left[-1,0] else -1].astype(np.int16), right=left[self.__count if left[0,0] < left[-1,0] else -self.__count-1].astype(np.int16))
     
     @staticmethod        
@@ -195,6 +239,7 @@ class ScaleDetector:
         return threshold < value2 - value1
     
 
+            
 '''
 for i in range(12):
     org = cv2.imread("Repository/Images/Image" + str(i+1) + ".jpg")
