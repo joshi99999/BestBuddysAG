c5ac2394b4973502f4b820f52233e83946f81bba
joshi99999 <juntjoshua@gmail.com>
2023-06-16 12:06:40 +0200

    update controller

--- position_controller/setup.py
+++ position_controller/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'control=position_controller.pos_control:main'
+            'control=position_controller.posController:main'
         ],
     },
 )
