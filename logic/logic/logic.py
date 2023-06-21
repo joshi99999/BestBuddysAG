38914bc4319d4aa4a981830a06fead7e9e3ec56c
seb <seb@bestbuddys.de>
2023-06-21 16:50:14 +0200

    Commit 2023.06.21 17:23

--- logic/logic/logic.py
+++ logic/logic/logic.py
@@ -2,25 +2,38 @@
 import rclpy
 import numpy as np
 from rclpy.node import Node
-from ro45_portalrobot_interfaces.msg import RobotPos, RobotCmd
+from ro45_portalrobot_interfaces.msg import PosVelClass, RobotPos
+
+POS_BOX_CAT = []
+POS_BOX_UNICORN = []
+POS_ERROR = [0, 0, 0]
+POS_WAIT = []
 
 class Controller(Node):
     def __init__(self):
         super().__init__('logic')
 
-        self.desired_pos = np.zeros(3, dtype=np.float32)
-        self.min_movement_x = 0.0
-        self.max_movement_x = 10.0
+        self.subscriber = self.create_subscription(PosVelClass, 'pos_vel_class', self.object_callback, 10)
+        self.subscriptionCurrent = self.create_subscription(RobotPos, 'robot_position', self.robotPostion_callback, 10)
+        self.publisher = self.create_publisher(RobotPos, 'robot_reference_position', 10)    
 
-        self.subscriptionDesired = self.create_subscription(RobotPos, 'robot_reference_position', self.desiredPosition_callback, 10)
-        self.publisher = self.create_publisher(RobotCmd, 'robot_command', 10)    
-        #var_pos_belt is only True if the robot is at a trayer
-        self.var_pos_trayer = False
-        #var_pos_trayer is only True if the robot is at the belt
-        self.var_pos_belt = False     
+        self.locked = False
+        self.position = np.zeros(2, dtype=np.float64)
+        self.velocity = 0.0
+        self.time = 0.0
+        self.type = None    #id: 0= Katze, 1 = Einhorn
 
-
-        #id: 0= Katze, 1 = Einhorn
+    def object_callback(self, msg):
+        if self.locked:
+            return
+        self.locked = True
+        self.position[:] = msg.pos_x, msg.pos_y
+        self.velocity = msg.vel_x
+        self.time = msg.time
+        self.type = msg.result
+    
+    def robotPosition_callback(self):
+        
 
     def desiredPosition_callback(self, msg):
         pos = self.desicion(msg.id)
