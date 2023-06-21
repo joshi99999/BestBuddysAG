205a90cebd741baf9e21876ab248456db80052a4
jere <jeremy.zid@stud.hs-kempten.de>
2023-06-21 15:48:20 +0200

    umrechnung in ms

--- gruppierung/gruppierung/modellierung.py
+++ gruppierung/gruppierung/modellierung.py
@@ -86,7 +86,7 @@ class PositionGruppierung(Node):
             return False, id, end_pos_x, end_pos_y, 0.0, end_time
         
         # pixel per second
-        pixel_time = (end_pos_x - start_pos_x) / (end_time - start_time)
+        pixel_time = (end_pos_x - start_pos_x) / ((end_time - start_time)*1000)
         # 25 pixel are equal to 1cm
         scale = 25
         # cm per second
