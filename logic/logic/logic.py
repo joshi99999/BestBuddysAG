b6ce43cd44ff3f7b17b5f250ba8388326de9fe1a
seb <seb@bestbuddys.de>
2023-06-27 17:28:03 +0200

    Commit 2023.06.27 17:27

--- logic/logic/logic.py
+++ logic/logic/logic.py
@@ -2,7 +2,7 @@
 import rclpy
 import numpy as np
 from rclpy.node import Node
-from ro45_portalrobot_interfaces.msg import PosVelClass, RobotPos
+from ro45_portalrobot_interfaces.msg import PosVelClass, RobotPos, ConCmd
 from time import time
 
 POS_BOX_CAT = []
@@ -18,18 +18,21 @@ class Controller(Node):
         self.subscriptionCurrent = self.create_subscription(RobotPos, 'robot_position', self.robotPostion_callback, 10)
         self.publisher = self.create_publisher(RobotPos, 'robot_reference_position', 10)    
 
-        self.locked = False
         self.error = False
-        self.pos_x = 0.0
-        self.pos_y = 0.0
-        self.velocity = 0.0
+        self.msg_out = ConCmd()
+
         self.time = 0.0
         self.type = None    #id: 0= Katze, 1 = Einhorn
-        self.state = 0
+        self.state = 'init'
         self.target = np.zeros(3, np.float64)
 
+        self.init_time = None
+        self.init_duration = 5
+        self.reference_position = np.zeros(3, dtype=np.float64)
+        self.position = np.zeros(2, dtype=np.float64)
+
     def object_callback(self, msg):
-        if self.locked or self.error:
+        if 0 < self.velocity or self.error:
             return
         self.locked = True
         self.position[:] = msg.pos_x, msg.pos_y
@@ -37,20 +40,64 @@ class Controller(Node):
         self.time = msg.time
         self.type = msg.result
     
-    def robotPosition_callback(self, msg):
+    def robotPosition_callback(self, msg_in):
         if self.error:
             return        
-    
-        position = np.array([msg.pos_x, msg.pos_y, msg.pos_z], dtype=np.float64)        
+        
+        self.msg_out.vel_x, self.msg_out.grip = .0, False
+
+        position = np.array([self.msg_out.pos_x, self.msg_out.pos_y, self.msg_out.pos_z], dtype=np.float64)
         if 0.001 < np.linalg.norm(self.target - position):
             return
 
         match self.state:
             case 'init':
+                if self.init_time is None:
+                    self.msg_out.pos_x, self.msg_out.pos_y = msg_in.pos_x, msg_in.pos_y
+                    self.init_time = time()
+                
+                if time() - self.init_time < self.init_duration:
+                    self.msg_out.pos_z = msg_in.pos_z - 0.01
+                    self.publisher.publish(self.msg_out)
+                else:
+                    self.init_time = None
+                    self.reference_position[3] = msg_in.pos_z
+                    self.state = 'reference_z'
+                
             case 'reference_z':
+                if self.init_time is None:
+                    self.msg_out.pos_z = msg_in.pos_z
+                    self.init_time = time()
+                
+                if time() - self.init_time < self.init_duration:
+                    self.msg_out.pos_x, self.msg_out.pos_y = msg_in.pos_x - 0.01, msg_in.pos_y - 0.01
+                    self.publisher.publish(self.msg_out)
+                else:
+                    self.reference_position[0:2] = msg_in.pos_x, msg_in.pos_y
+                    self.state = 'reference'
+
             case 'reference':
+                self.msg_out.pos_x, self.msg_out.pos_y, self.msg_out.pos_z = POS_WAIT
+                self.publisher.publish(self.msg_out)
+                self.state = 'wait'
+
             case 'wait':
-            case 'object_xy':
+                if 0 < self.msg_out.vel_x:
+                    self.state = 'fetch'
+            
+            case 'fetch_yz':
+                x = self.position[0] + (time() - self.time) * self.msg_out.vel_x
+                if 0.001 < np.linalg.norm(x - [self.msg_out.pos_x, self.msg_out.pos_y, self.msg_out.pos_z]):
+                    self.msg_out.pos_x, self.msg_out.pos_y = x
+                    self.msg_out.pos_z = POS_WAIT[2]
+                    self.publisher.publish(self.msg_out)
+                    self.msg_in.pos_y = 
+                else:
+
+            case 'fetch_x':
+
+            case 'object_yz':
+                
             case 'object':
             case 'lifted':
             case 'y_enabled':
