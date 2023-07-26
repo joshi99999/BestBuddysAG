e47351282181c64310b5bfdae3f8cc912218dd49
Fabrice Cheteu <fabricecheteu332@gmail.com>
2023-07-26 21:32:15 +0200

    docu

--- classification/classification/Classification.py
+++ classification/classification/Classification.py
@@ -13,14 +13,11 @@ import joblib
 from joblib import dump , load
 from sklearn import svm
 
-
+## A Objektclassification is used to classifier a objekt . 
 class ObjectClassification(Node):
-
+    
+    ##Initializes the object classification.
     def __init__(self):
-        """
-        Initializes the object classification.
-
-        """
         super().__init__('ObjectClassification')
         self.subscription = self.create_subscription(IdSample, 'id_sample', self.image_callback, 10)
         self.publisher = self.create_publisher(IdClassVec, 'id_class_vec', 10)
@@ -30,14 +27,9 @@ class ObjectClassification(Node):
         self.svm_model = load(model_file)
         
         
-
+    ##Callback function for the input image.
+    # @param Image: ROS image.
     def image_callback(self, IdSample):
-        """
-        Callback function for the input image.
-
-        Parameters:
-            Image: ROS image.
-        """
         cv_image = self.bridge.imgmsg_to_cv2(IdSample.image, desired_encoding='8UC1')
         features, vector = self.feature_extract(cv_image)
         """print(features)
@@ -68,6 +60,9 @@ class ObjectClassification(Node):
         
         self.publisher.publish(msg)
     
+    ##Finds the centroid (center of mass) of the largest object in a binary image.
+    # @param image (ndarray): The binary image containing objects.
+    # @return tuple or None: A tuple representing the centroid coordinates (cx, cy) of the largest object. If no objects are found (no contours detected), it returns None.
     def find_center_of_mass(self, image):
         # Finden der Konturen der Objekte im binären Bild
         contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -91,25 +86,21 @@ class ObjectClassification(Node):
         # Rückgabe des Schwerpunkts (Centroid)
         return cx, cy
     
-    def find_gripping_point_vector(self, gripping_point, center_point):
+    #Calculates the vector between a gripping point and a center point.
+    # @param gripping_point (tuple): The coordinates of the gripping point.
+    # @param center_point (tuple): The coordinates of the center point.
+    # @return  A tuple representing the vector from the center point to the gripping point. The tuple contains the x-component and y-component of the vector.             
+    def find_gripping_point_vector(self, gripping_point, center_point):   
+    # Calculate the vector components
+    vector = (gripping_point[0] - center_point[0], gripping_point[1] - center_point[1])
+    return vector
 
-        vector = (gripping_point[0] - center_point[0], gripping_point[1] - center_point[1])
-
-        return vector
-
+    ##Classifies an image as a cat or unicorn.
+    # @param image (str): Path to the image.
+    # @return features (list): List of extracted features of the image.
+    # @return gripping_point : A tuple containing the gripping point as (x, y) coordinates.
+    # @return gravity : List containing the gravity_x and the gravity_y
     def feature_extract(self, image):
-        """
-        Classifies an image as a cat or unicorn.
-
-        Parameters:
-            image (str): Path to the image.
-
-        Returns:
-            features (list): List of extracted features of the image.
-            gripping_point : A tuple containing the gripping point as (x, y) coordinates.
-            gravity : List containing the gravity_x and the gravity_y
-            
-        """
         try:
             # Gripping Point
             transformed = cv2.distanceTransform(image, cv2.DIST_L2, 3)
@@ -136,11 +127,6 @@ class ObjectClassification(Node):
                 for j in range(dst.shape[1]):
                     if dst[i, j] > threshold:
                         corners.append((i, j))
-            ''''
-            gravity_y, gravity_x = center_of_gravity(binary_img)
-            gravity =[gravity_x, gravity_y]
-            cv2.circle(edges, (int(gravity_x + 0.5), int(gravity_y + 0.5)), 5, (0, 0, 255), -1)
-            '''
             e_vals, _ = np.linalg.eig(inertia_tensor(binary_img))
             try:
                 I_x, I_y = Hauptmoment(e_vals)
@@ -153,36 +139,11 @@ class ObjectClassification(Node):
         except cv2.error as e:
             print("bild can not load:", str(e))
             return None
-
-
-
-
-def main(args=None):
-    """
-    Main function to start object classification
-    
-    """
-    rclpy.init(args=args)
-    object_classification = ObjectClassification()
-    rclpy.spin(object_classification)
-    object_classification.destroy_node()
-    rclpy.shutdown()
-
-if __name__ == '__main__':
-    main()
-
-   
+        
+##Calculates the centroid of a binary image.
+# @param img (ndarray) the binary image.
+# @return np.ndarray: the centroid coordinates (y, x) of the image .
 def center_of_gravity(img):
-    """
-    Calculates the centroid of a binary image.
-
-    Parameters:
-        img (ndarray): Binary image.
-
-    Returns:
-        np.ndarray: Centroid coordinates (y, x).
-
-    """
     try:
         yind, xind = np.where(img)
         return np.array((np.mean(yind), np.mean(xind)))
@@ -190,18 +151,10 @@ def center_of_gravity(img):
         print("gracity can not calculated:", str(e))
         return None
 
-
+##Calculates the inertia tensor of a binary image.
+# @param binary_img (ndarray) a binary image.
+# @return np.ndarray Inertia tensor matrix.
 def inertia_tensor(binary_img):
-    """
-    Calculates the inertia tensor of a binary image.
-
-    Parameters:
-        binary_img (ndarray): Binary image.
-
-    Returns:
-        np.ndarray: Inertia tensor matrix.
-
-    """
     try:
         gravity_y, gravity_x = center_of_gravity(binary_img)
         matrix_inertia_tensor = np.zeros((2, 2))
@@ -217,20 +170,11 @@ def inertia_tensor(binary_img):
     except Exception as e:
         print("inertia_tensor can not calculated:", str(e))
         return None
-  
-   
+
+##Calculates the principal moments based on the eigenvalues.
+# @param e_vals (ndarray): Eigenvalues.
+# @return I_x (float): Principal moment along the x-axis  and  I_y (float): Principal moment along the y-axis. 
 def Hauptmoment(e_vals):
-    """
-    Calculates the principal moments based on the eigenvalues.
-
-    Parameters:
-        e_vals (ndarray): Eigenvalues.
-
-    Returns:
-        I_x (float): Principal moment along the x-axis.
-        I_y (float): Principal moment along the y-axis.
-
-    """
     try:
         major_eigenvalue_idx = np.argmax(e_vals)
         minor_eigenvalue_idx = np.argmin(e_vals)
@@ -243,19 +187,11 @@ def Hauptmoment(e_vals):
         print("Invalid input. The 'e_vals' parameter must be a valid ndarray.")
         return None, None
 
-
+##Performs a prediction using the SVM model and prints the result.
+# @param features (list): List of image features.
+# @param model: SVM model.
+# @return the prediction_class (0 for cat, 1 for unicorn).
 def pred(features, model):
-    """
-    Performs a prediction using the SVM model and prints the result.
-
-    Parameters:
-        features (list): List of image features.
-        model: SVM model.
-
-    Returns:
-        int: Prediction class (0 for cat, 1 for unicorn).
-
-    """
     try:
         prediction = model.predict([features])[0]
         if prediction not in [0, 1]:
@@ -264,3 +200,14 @@ def pred(features, model):
     except ValueError as e:
         print("Error during prediction:", str(e))
         return -1
+
+##Main function to start object classification 
+def main(args=None):
+    rclpy.init(args=args)
+    object_classification = ObjectClassification()
+    rclpy.spin(object_classification)
+    object_classification.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
--- modeling/modeling/modeling.py
+++ modeling/modeling/modeling.py
@@ -4,13 +4,13 @@ from rclpy.node import Node
 from ro45_portalrobot_interfaces.msg import IdPosVelTime, IdPosTime
 import numpy as np
 import cv2
-    
-class Modeler(Node):
-    def __init__(self):
-        """
-        Initializes the PositionGrouping node.
 
-        """
+## A Modeler is used to determine the speed of the objket on the conveyor belt.
+
+class Modeler(Node):
+    
+    ##Initializes the PositionGrouping node.
+    def __init__(self):
         super().__init__('modeler')
 
         self.old_ids = []        
@@ -20,93 +20,96 @@ class Modeler(Node):
 
         self.subscriber= self.create_subscription(IdPosTime, 'id_pos_time', self.position_callback, 10)
         self.publisher = self.create_publisher(IdPosVelTime, 'id_pos_vel_time', 10)
-            
-    def position_callback(self, msg_in):
-        """
-        Callback function that is called when an IdPosTime message is received.
-
-        parameters:
-            IdPosTime: The received Id, Position  and Time.
-
-        """
-        if msg_in.id.data in self.old_ids:
-            return
-        
-        id = self.add_position(msg_in.id.data, msg_in.time.data, msg_in.pos_x.data, msg_in.pos_y.data )
-
-        if id is not None:
-            positions = self.get_positions_by_id(id)
-            ret = Modeler.calculate_movement(positions)
-            if ret is not None:
-                msg_out = IdPosVelTime()
-                msg_out.id = id
-                msg_out.pos_x, msg_out.pos_y, msg_out.vel_x, msg_out.time = ret
-                self.get_logger().info('Publishing: "%s"' % msg_out)
-                self.publisher.publish(msg_out)
-
-    #Alternate function to speed_determine
     
+    ##This callback function is called automatically when an IdPosTime message is received. It processes the received message, adding the position and time data to the internal storage. If the Id is new, it calculates the movement from stored positions and publishes the result.
+    # @param  msg_in (IdPosTime): The received IdPosTime message containing the Id, Position, and Time data.
+    # @return none
+    def position_callback(self, msg_in):
+        try:
+            if msg_in.id.data in self.old_ids:
+                return
+
+            id = self.add_position(msg_in.id.data, msg_in.time.data, msg_in.pos_x.data, msg_in.pos_y.data)
+
+            if id is not None:
+                positions = self.get_positions_by_id(id)
+                ret = Modeler.calculate_movement(positions)
+                if ret is not None:
+                    msg_out = IdPosVelTime()
+                    msg_out.id = id
+                    msg_out.pos_x, msg_out.pos_y, msg_out.vel_x, msg_out.time = ret
+                    self.get_logger().info('Publishing: "%s"' % msg_out)
+                    self.publisher.publish(msg_out)
+        except Exception as e:
+            # Catch any unexpected exceptions that may occur within the method
+            print(f"An unexpected error occurred in position_callback: {e}")
+
+    ##Calculates movement data from a list of points.
+    # @param list (List): A list of points, where each point is represented as a 3-tuple (x, y, t). Here, x denotes the horizontal position, y denotes the vertical position, and t denotes time.
+    # @return Tuple: A 4-tuple containing the calculated movement data: (shifted_horizontal_position, vertical_position, velocity, shifted_time)
     @staticmethod 
     def calculate_movement(list):
-        if len(list) < 2:
-            return
-        array = np.array(list, dtype=np.float64)
-        y = np.average(array[:,2]).astype(np.float64) 
-        offset = int(min(array[:,0])/100000)*100000
-        array[:,0] = array[:,0] - offset
-        vx, vy, t, x = cv2.fitLine(array[:,0:2], distType=cv2.DIST_L2, param = 0, reps = 0.01, aeps = 0.01).flatten().astype(np.float64)
-        v = vy/vx
-        return x*0.0004, y*0.0004, v*0.4, int(t+offset)
-
+        try:
+            if len(list) < 2:
+                raise ValueError("At least two points are required to calculate movement.")
+            array = np.array(list, dtype=np.float64)
+            y = np.average(array[:,2]).astype(np.float64) 
+            offset = int(min(array[:,0])/100000)*100000
+            array[:,0] = array[:,0] - offset
+            vx, vy, t, x = cv2.fitLine(array[:,0:2], distType=cv2.DIST_L2, param = 0, reps = 0.01, aeps = 0.01).flatten().astype(np.float64)
+            v = vy/vx
+            return x*0.0004, y*0.0004, v*0.4, int(t+offset)
+        except ValueError as e :
+            print(str(e))
+            return None
+        except:
+            # Catch any other unexpected exceptions that may occur
+            return None
+    
+    ##Removes all items with the specified ID from a list and returns them as a separate list.
+    # @param  list_for_element (list): The list to remove elements from.
+    # @param id: The ID of the items to be removed.
+    # @return list: A separate list containing the removed items.
     def get_positions_by_id(self, id):
-        """
-        Removes all items with the specified ID from a list and returns them as a separate list.
-
-        parameters:
-            list_for_element (list): The list to remove elements from.
-            id: The ID of the items to be removed.
-
-        returns:
-            list: A separate list containing the removed items.
-
-        """
-        self.id_list.index(id)
-        positions = self.position_lists.pop(self.id_list.index(id))
-        self.id_list.remove(id)
-        self.old_ids.append(id)
-        return positions
+        try:
+            index = self.id_list.index(id)
+            positions = self.position_lists.pop(index)
+            self.id_list.remove(id)
+            self.old_ids.append(id)
+            return positions
+        except ValueError as e :
+            # Catch the ValueError if the specified ID is not found in the id_list
+            raise ValueError("ID not found in the list:", str(e))
         
+    ##Adds a newelement to the list , sorts the list by ID and timestamp and Checks if the object is still in the tracking area.
+    # @param id: The ID of the new element.
+    # @param timestamp: The timestamp of the new element.
+    # @param pos_x: The x-position of the new element.
+    # @param pos_y: The y-position of the new element.
+    # @return None
     def add_position(self, id , timestamp, pos_x, pos_y):
-        """
-        Adds a new element to the list , sorts the list by ID and timestamp and Checks if the object is still in the tracking area.
-
-        parameters:
-            id: The ID of the new element.
-            timestamp: The timestamp of the new element.
-            pos_x: The x-position of the new element.
-            pos_y: The y-position of the new element.
-
-        returns:
-            None
-            
-        """
-
         max_pos_x = 710
-
-        if pos_x >= max_pos_x:
-            return id
         
-        if id not in self.id_list:
-            self.id_list.append(id)
-            self.position_lists.append([[timestamp, pos_x, pos_y]])
-        else:
-            self.position_lists[self.id_list.index(id)].append([timestamp, pos_x, pos_y])
-            
-def main(args=None):
-    """
-    Main function to start object classification.
+        try:
+            # Check if the x-position is within the tracking area
+            if pos_x >= max_pos_x:
+                raise ValueError("Invalid position: x-position is outside the tracking area.")
 
-    """
+            # Validate other parameters if needed
+            # For example, you might want to check if the provided timestamp, pos_x, or pos_y are valid.
+
+            if id not in self.id_list:
+                self.id_list.append(id)
+                self.position_lists.append([[timestamp, pos_x, pos_y]])
+            else:
+                self.position_lists[self.id_list.index(id)].append([timestamp, pos_x, pos_y])
+
+        except ValueError as e:
+            # Catch the ValueError and handle it within the method
+            print("Error: ", str(e))
+            
+##Main function to start object classification.
+def main(args=None):
     rclpy.init(args=args)
     modeler = Modeler()
     rclpy.spin(modeler)
