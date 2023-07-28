6adabab761504b29536952f73b4abad279aca45c
jere <jeremy.zid@stud.hs-kempten.de>
2023-07-28 15:09:41 +0200

    commit 15:09

--- detection_node/detection_node/tracker.py
+++ detection_node/detection_node/tracker.py
@@ -9,7 +9,17 @@ from ro45_portalrobot_interfaces.msg import IdSample, IdPosTime
 
 from cv_bridge import CvBridge
 
+## A EuclideanDistTracker instance is used to track and detected Objects, in an image.
+#
+#  To work properly the following conditions must be met.
+#  + The given image must be a binary image.
+#  + Objects need to be big enough.
+#  + Objects need to be bright enough.
+#  + Objects should not contact each other to detect an track them properly
+
 class EuclideanDistTracker:
+
+    ## Initializes a new EuclideanDistTracker instance.
     def __init__(self):
         # Store the center positions of the objects
         self.center_points = {}
@@ -19,6 +29,9 @@ class EuclideanDistTracker:
 
         self.cv_bridge = CvBridge()
 
+    ## Updates the euclideandisttracker to track objects
+    # @param objects_rect A list which contains the x and y coordinates and the width and heigth of the bounding boxes 
+    # @return obcets_bbs_ids A list which contains the ids and the boundings boxes of the tracked objects 
     def update(self, objects_rect):
         """
         Updates the object tracking based on the given object rectangles.
@@ -68,16 +81,14 @@ class EuclideanDistTracker:
         self.center_points = new_center_points.copy()
         return objects_bbs_ids
 
-    
-    def getSample(self, frame, center_x, cy, id):
-        """
-        This function creates an slice of an image at a certain position if the x coordinate of an object is there
 
-        Args:
-            image (Numpy Array), x coordinate of object (int), id of object (int)
-        Returns:
-            image (Numpy Array), id of object (int)
-        """
+    ## Creates an image of an object if it is in a certain range
+    # @param frame The image with objects of which samples should be taken
+    # @param center_x The x-coordinate of the object center point
+    # @param id The id of the object
+    # @return publish A boolean value that is true if a sample was taken and false if not
+    # @return id_img A ros2 msg which contains the id of the object and the taken sample of the object
+    def getSample(self, frame, center_x, id):
         publish = False
         if (frame is not None):
             id_img = IdSample()
@@ -111,17 +122,10 @@ class EuclideanDistTracker:
                 return publish, id_img
             
     
+    ## Detects objects in a given image 
+    # @param frame The input image 
+    # @return detections A list which contains the x and y coordinates and the width and heigth of the bounding boxes 
     def detectObject(self, frame):
-        """
-        Detects objects in the given frame.
-
-        Args:
-            frame: The input frame to detect objects from.
-
-        Returns:
-            list: A list of detected object rectangles (x, y, w, h).
-
-        """
         detections = []
         contours, _=cv2.findContours(frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
         for cnt in contours:
@@ -133,18 +137,12 @@ class EuclideanDistTracker:
                 detections.append([x, y, w, h])
         return detections
     
-    
+    ## Tracks objects by using update function
+    # @param detections A list which contains the x and y coordinates and the width and heigth of the bounding boxes
+    # @return id The id of the tracked object
+    # @return cx The x-coordinate of the object center point
+    # @return cy the y-coordinate of the object center point
     def trackObject(self, detections):
-        """
-        Tracks an object based on the given detections.
-
-        Args:
-            detections (list): A list of detected object rectangles (x, y, w, h).
-
-        Returns:
-            tuple: A tuple containing the ID of the tracked object, and its center coordinates (center_x, cy).
-
-        """
         if (detections is not None) and (len(detections)>0):
             boxes_ids = self.update(detections)
             for box_id in boxes_ids:
@@ -155,28 +153,30 @@ class EuclideanDistTracker:
                 return id, cx, cy
         else: return -1, 0, 0
     
-
+    ## Calculates the center of mass of an image
+    # @param image The image 
+    # @param x The offset in x direction
+    # @param y The offset in y direction
     def find_center_of_mass(self, image, x, y):
-        # Finden der Konturen der Objekte im binären Bild
+        # Find contours in image
         contours, _ = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Falls keine Konturen gefunden wurden, gibt es keine Objekte
+        # if there are no contours return None
         if len(contours) == 0:
             return None
 
-        # Finden des größten Objekts basierend auf der Konturfläche
+        # find largest contour
         largest_contour = max(contours, key=cv2.contourArea)
 
-        # Berechnung der Momente des größten Objekts
+        # calculate momentum
         moments = cv2.moments(largest_contour)
 
-        # Berechnung des Schwerpunkts (Centroid) des größten Objekts
+        # calculate center of mass
         centroid_x = int(moments['m10'] / moments['m00'])
         centroid_y = int(moments['m01'] / moments['m00'])
         cx = centroid_x + x
         cy = centroid_y + y
 
-        # Rückgabe des Schwerpunkts (Centroid)
         return cx, cy
 
 
